86363ea1e2c0353985d9b59879f79a3bdc296e6a
André Tissot <andrehtissot@gmail.com>
2013-09-24 12:00:02 -0300

    already works

---
+++ JournalDataParser/parse_computing.rb
@@ -0,0 +1,9 @@
+QUALIS_PATH = File.absolute_path(File.dirname(__FILE__)+'/../')
+require 'csv'
+require "#{QUALIS_PATH}/JournalDataParser/qualisjournaldataparser.rb"
+
+puts "Start parsing Qualis..."
+QualisJournalDataParser.new.generate_csv "#{QUALIS_PATH}/downloaded/Consulta_Webqualis.ps", "#{QUALIS_PATH}/journal_qualis_computing.csv",
+	{verbose:true, filter:'CIÊNCIA DA COMPUTAÇÃO'}
+puts "Stop parsing Qualis."
+exit
